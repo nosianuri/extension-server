1d3cf9f9677d5687144effa60c4e4b2c7875c5a1
nosianuri <nurinosia@gmail.com>
2022-10-25 19:01:19 +0600

    added user data with mongodb

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// user_extension || 2fWGX37OcC1wXT4b
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0j7e020.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("extensionApp").collection("user");
+
+        // User API
+        app.get('/user', async (req, res) => {
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const query = {};
+            const cursor = userCollection.find(query);
+            let users;
+            if(page || size){
+                users = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                users = await cursor.toArray();
+            }
+            
+            res.send(users);
+        });
+
+        app.get('/userCount', async(req, res) =>{
+            // const query = {};
+            // const cursor = userCollection.find(query);
+            const count = await userCollection.estimatedDocumentCount();
+            res.send({count});
+        })
+
+        // app.get('/user/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running My extension server');
+});
+
+app.listen(port, () => {
+    console.log('CRUD Server is running');
+})
